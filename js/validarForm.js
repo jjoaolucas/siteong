2615fcea36cc2aa3ac33b267ff2e1711acfb8f9e
jjoaolucas <joaolucas-05@outlook.com>
2023-11-26 14:30:16 -0300

    feats: acrescentado mudanças do feedback chegagem

--- js/validarForm.js
+++ js/validarForm.js
@@ -37,6 +37,8 @@
       var cartao = document.getElementById('numeroCartao').value;
       var cvv = document.getElementById('cvv').value;
       var cpf = document.getElementById('cpf').value;
+      var mes = document.getElementById('mes').value;
+      var ano = document.getElementById('ano').value;
 
       if (!regexNome.test(nome)) {
         alert('Por favor, insira um nome válido.');
@@ -76,8 +78,59 @@
         alert('Por favor, insira um CPF válido.');
         return false;
       }
-
-      // Se passar por todas as validações
+      if (mes > 12 || mes < 0 ){
+        alert('Por favor, insira um MES válido.');
+        return false;
+      }
+      if (ano < 2023){
+        alert('Por favor, insira um ANO DE EXPIRAÇÃO válido.');
+        return false;
+      }
+      
       alert('Obrigado por fazer a diferença! Sua doação foi enviada com sucesso!');
       return true;
     }
+function validarCPF() {
+      var cpf = event.target.value;
+      var ok = 1;
+      var add;
+      if (cpf != "") {
+         cpf = cpf.replace(/[^\d]+/g, '');
+         if (cpf.length != 11 ||
+            cpf == "00000000000" ||
+            cpf == "11111111111" ||
+            cpf == "22222222222" ||
+            cpf == "33333333333" ||
+            cpf == "44444444444" ||
+            cpf == "55555555555" ||
+            cpf == "66666666666" ||
+            cpf == "77777777777" ||
+            cpf == "88888888888" ||
+            cpf == "99999999999")
+                ok = 0;
+         if (ok == 1) {
+            add = 0;
+            for (i = 0; i < 9; i++)
+               add += parseInt(cpf.charAt(i)) * (10 - i);
+               rev = 11 - (add % 11);
+               if (rev == 10 || rev == 11)
+                  rev = 0;
+               if (rev != parseInt(cpf.charAt(9)))
+                  ok = 0;
+               if (ok == 1) {
+                  add = 0;
+                  for (i = 0; i < 10; i++)
+                     add += parseInt(cpf.charAt(i)) * (11 - i);
+                  rev = 11 - (add % 11);
+                  if (rev == 10 || rev == 11)
+                     rev = 0;
+                  if (rev != parseInt(cpf.charAt(10)))
+                     ok = 0;
+               }
+           }
+           if (ok == 0) {
+              alert("Ops... Ocorreu um problema... CPF inválido!");
+              //event.target.focus();
+           }
+       }
+   }
